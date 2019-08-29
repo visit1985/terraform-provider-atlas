c65f9369fba49a390aa0861f7925c30063c18c24
Michael Goehler <michael.goehler@lpsolutions.com>
2019-08-29 19:11:01 +0200

    restructuring

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/visit1985/terraform-provider-atlas/atlas"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return atlas.Provider()
 		},
 	})
 }
